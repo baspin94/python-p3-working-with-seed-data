752ca4b9f1946b77a4064d7944efa05bee7d3617
Bianca Aspin <bianca@mj94.net>
2023-03-05 23:53:04 -0800

    Complete lab.

--- lib/seed.py
+++ lib/seed.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
-
-from faker import Faker
-import random
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from models import Game
 
+from faker import Faker
+import random
 fake = Faker()
 
 if __name__ == '__main__':
@@ -14,3 +13,23 @@ if __name__ == '__main__':
     engine = create_engine('sqlite:///seed_db.db')
     Session = sessionmaker(bind=engine)
     session = Session()
+    
+    # Clear out any data that already exists in the table 
+    # before re-running seed file.
+    session.query(Game).delete()
+    session.commit()
+
+    print("Seeding games...")
+
+    # Adding seed data
+    games = [
+        Game(
+            title=fake.word(),
+            genre=fake.word(),
+            platform=fake.word(),
+            price=random.randint(0, 60)
+        )
+    for i in range(50)]
+    
+    session.bulk_save_objects(games)
+    session.commit()
